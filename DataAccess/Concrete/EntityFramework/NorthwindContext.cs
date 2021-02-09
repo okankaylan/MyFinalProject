b5ac4d7d03b5d4726cbb7198dc8572a1bc8355b4
Zok <okankaylan@hotmail.com>
2021-02-09 22:02:56 +0300

    Ef northwind db implementation

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +9,12 @@ namespace DataAccess.Concrete.EntityFramework
     //Context : Db tabloları ile proje classlarını bağlamak
     public class NorthwindContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
     }
 }
