9e74d979506d299495b6ebb7a05d10d58baa6c10
Zok <okankaylan@hotmail.com>
2021-02-09 20:35:05 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+    }
+}
